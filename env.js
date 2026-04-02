de9b34156744081c4adbea52941a8ab6cdabbec4
Gary Sonnenberg <garysonnenberg@MacBook-Pro.local>
2026-04-02 08:51:09 -0500

    Initial commit

---
+++ env.js
@@ -0,0 +1,6 @@
+// env.js
+window.ENV = {
+  SUPABASE_URL: "https://onilozlryvymirztckxg.supabase.co",
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uaWxvemxyeXZ5bWlyenRja3hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNjQ1MjMsImV4cCI6MjA4ODk0MDUyM30.VZPZTATCSHPNuN8WdbP-3qiY6WQelcGsjofcI7HF-gI",
+};
